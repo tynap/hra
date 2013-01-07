e0228391d512f2dacc001a651637347085711692
Jirka Hudec <jurahudec@gmail.com>
2013-01-07 04:57:16 +0100

    opravené použij

--- zdrojaky/cz/vse/_101/ut0915/xpelk04/CommandPouzij.java
+++ zdrojaky/cz/vse/_101/ut0915/xpelk04/CommandPouzij.java
@@ -5,10 +5,11 @@ package cz.vse._101.ut0915.xpelk04;
 
 
 
-/*******************************************************************************
+/**
+ * *****************************************************************************
  * Instances of class {@code CommandPouzij} represent ...
  *
- * @author  Rudolf PECINOVSKÝ
+ * @author Rudolf PECINOVSKÝ
  * @version 0.00.0000 — 20yy-mm-dd
  */
 public class CommandPouzij extends ACommand
@@ -20,78 +21,73 @@ public class CommandPouzij extends ACommand
 //== VARIABLE INSTANCE ATTRIBUTES ==============================================
 //== CLASS GETTERS AND SETTERS =================================================
 //== OTHER NON-PRIVATE CLASS METHODS ===========================================
-
 //##############################################################################
 //== CONSTUCTORS AND FACTORY METHODS ===========================================
-
-    /***************************************************************************
+    /**
+     * *************************************************************************
      *
      */
     public CommandPouzij()
     {
         super("pouzij", "Příkaz, který použije vámi vybranou věc a něco " +
-                "vám za ní dá/něco se vám otevře atd..");
+                        "vám za ní dá/něco se vám otevře atd..");
     }
-
 
 
 //== ABSTRACT METHODS ==========================================================
 //== INSTANCE GETTERS AND SETTERS ==============================================
 //== OTHER NON-PRIVATE INSTANCE METHODS ========================================
-
-     @Override
+    @Override
     public String execute(String... arguments)
     {
-        if (arguments.length < 2)
-          {
+        if (arguments.length < 2) {
             return "Nezadali jste objekt, který chcete nastražit" + status();
-          }
-
-      String somethingName = arguments[1];
-      Hands HANDS = Hands.getInstance();
-      Place currentPlace = Place.getCurrentPlace();
-
-      boolean jeVoda    = false;
-      boolean jeKarta   = false;
-
-        if (!somethingName.toLowerCase().equals("voda") ||
-            !somethingName.toLowerCase().equals("ošetřovatelova_karta"))
-                {
-                    return  "Tento předmět nemůžete použít." +  status();
-                }
-
-
-        Something voda = HANDS.chooseSomething("Voda");
-        if (somethingName != null) {
-            jeVoda = true;
         }
 
-        Something karta = HANDS.chooseSomething("Ošetřovatelova_karta");
-        if (somethingName != null) {
-            jeKarta = true;
+        String somethingName = arguments[1];
+        Hands HANDS = Hands.getInstance();
+        Place currentPlace = Place.getCurrentPlace();
+
+        boolean jeVoda = false;
+        boolean jeKarta = false;
+
+        if (somethingName.toLowerCase().equals("voda") ||
+            somethingName.toLowerCase().equals("ošetřovatelova_karta")) {
+
+            Something voda = HANDS.chooseSomething("Voda");
+            if (somethingName != null) {
+                jeVoda = true;
+            }
+
+            Something karta = HANDS.chooseSomething("Ošetřovatelova_karta");
+            if (somethingName != null) {
+                jeKarta = true;
+            }
+
+            if (jeVoda && currentPlace.getName().equals("Sloni")) {
+                currentPlace.add(Something.newOrdinarySomething(
+                        "Ošetřovatelova_karta"));
+                HANDS.remove(voda);
+                return status();
+
+            }
+
+            if (jeKarta && currentPlace.getName().equals("Exit")) {
+                Game.getInstance().stop();
+                return
+                "\"Cvak\", slyšíte odedveří, které se pomalu otevírají a vy " +
+                "bezpečně v plném zdraví vycházíte ze ZOO, spíše jungle a venku" +
+                "na vás čekají davy novinářů. Děkuji, že jste si úspěšně " +
+                "zahrál(a) moji hru.";
+            }
+        }
+        else {
+            return "Tento předmět nemůžete použít." + status();
         }
 
 
-        if (jeVoda && currentPlace.getName().equals("Sloni"))
-        {
-            currentPlace.add(Something.newOrdinarySomething(
-                    "Ošetřovatelova karta"));
-            HANDS.remove(voda);
-
-        }
-
-
-
-        if (jeKarta && currentPlace.getName().equals("Exit"))
-        { Game.getInstance().stop();
-            return
-            "\"Cvak\", slyšíte odedveří, které se pomalu otevírají a vy " +
-            "bezpečně v plném zdraví vycházíte ze ZOO, spíše jungle a venku" +
-            "na vás čekají davy novinářů. Děkuji, že jste si úspěšně " +
-            "zahrál(a) moji hru." ;
-        }
- return "";
- }
+        return "";
+    }
 
     //== PRIVATE AND AUXILIARY CLASS METHODS =======================================
 //== PRIVATE AND AUXILIARY INSTANCE METHODS ====================================
@@ -108,3 +104,5 @@ public class CommandPouzij extends ACommand
 //    /** @param args Command line arguments - not used. */
 //    public static void main(String[] args)  {  test();  }
 }
+
+
